77d9df4f77957c93b7b9891db9ebc1c2b348a758
gregogun <gregogun97@gmail.com>
2023-02-06 17:55:38 +0000

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,15 @@
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+  reactStrictMode: false,
+  trailingSlash: true,
+  images: {
+    domains: ["arweave.net"],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
+module.exports = nextConfig;
+  
