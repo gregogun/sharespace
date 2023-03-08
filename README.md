31230e7adfdf37fcb3501d7d606dc3b1d889a194
gregogun <gregogun97@gmail.com>
2023-03-08 14:00:40 +0000

    feat: :sparkles: add stamp feature

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A permaweb app that allows individuals to share their ideas and explorations.
 
 - [x] Users can create/post an idea
 - [x] Users can view posts in a feed
-- [ ] Users can stamp an idea
+- [x] Users can stamp an idea
 - [ ] Users can create/post a project/exploration
 - [ ] Users can link ideas to their implementations
 - [ ] Users can fork/extend ideas
--- modules/feed/IdeaCard.tsx
+++ modules/feed/IdeaCard.tsx
@@ -1,15 +1,53 @@
+import { useAuth } from "@/hooks/useAuth";
+import { StampIcon } from "@/icons/StampIcon";
+import { stampAsset, userHasStamped } from "@/lib/stamps";
 import { Post } from "@/types";
 import { Image } from "@/ui/Image";
 import { getAccountAvatar, timestampToTimeAgo } from "@/utils";
 import { Box, Flex, Typography, Button } from "@aura-ui/react";
+import { useEffect, useState } from "react";
 
 interface IdeaCardProps {
   post: Post;
 }
 
 export const IdeaCard = ({
-  post: { creator, title, description, timestamp },
+  post: { creator, title, description, timestamp, id, stamps },
 }: IdeaCardProps) => {
+  const [justStamped, setJustStamped] = useState(false);
+  const [hasStamped, setHasStamped] = useState(false);
+  const { walletAddress } = useAuth();
+
+  useEffect(() => {
+    checkHasStamped();
+  }, []);
+
+  const checkHasStamped = async () => {
+    if (!walletAddress) return;
+    await userHasStamped(walletAddress, id)
+      .then((res) => {
+        console.log(res);
+        setHasStamped(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const isLoggedIn =
+    walletAddress && walletAddress === creator.address ? true : false;
+
+  const handleClick = async () => {
+    await stampAsset(id)
+      .then((res) => {
+        console.log(res);
+        setJustStamped(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Box css={{ p: "$5", backgroundColor: "$slate2", br: "$3" }}>
       <Typography contrast="hiContrast" css={{ mb: "$1" }} size="5" weight="6">
@@ -18,8 +56,54 @@ export const IdeaCard = ({
       <Typography css={{ mb: "$10" }} size="3">
         {description}
       </Typography>
-      <Flex justify="end" align="center">
-        <Button>Stamp</Button>
+      <Flex justify="between" align="center">
+        {isLoggedIn ? (
+          <Typography css={{ display: "flex", gap: "$1", color: "$blue11" }}>
+            Stamps
+            <Flex
+              as="span"
+              align="center"
+              justify="center"
+              css={{
+                px: "$3",
+                py: "$1",
+                backgroundColor: "$blue9",
+                color: "white",
+                br: "$pill",
+                fontSize: "$1",
+                lineHeight: 1,
+              }}
+            >
+              {stamps}
+            </Flex>
+          </Typography>
+        ) : (
+          <Button
+            onClick={handleClick}
+            variant="ghost"
+            colorScheme={justStamped ? "green" : "blue"}
+            disabled={hasStamped}
+          >
+            {/* <StampIcon /> */}
+            {justStamped || hasStamped ? `Stamped` : `Stamp`}
+            <Flex
+              as="span"
+              align="center"
+              justify="center"
+              css={{
+                px: "$2",
+                py: "$1",
+                backgroundColor: "$blue9",
+                color: "white",
+                br: "$pill",
+                fontSize: "$1",
+                lineHeight: 1,
+              }}
+            >
+              {stamps}
+            </Flex>
+          </Button>
+        )}
         <Flex gap="2" align="center">
           <Image
             css={{
